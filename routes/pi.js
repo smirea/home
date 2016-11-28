3e7d7f08b1a10aa78c83eaeb6d7c2fd076ef4cb7
Stefan Mirea <steven.mirea@gmail.com>
2016-11-27 22:55:04 -0500

    Updated pi post

--- routes/pi.js
+++ routes/pi.js
@@ -1,6 +1,7 @@
 
-const express = require('express');
+
 const {exec} = require('child_process');
+const express = require('express');
 
 const {getVar} = require('../utils/var');
 const logger = require('../utils/logger');
@@ -10,7 +11,7 @@ module.exports = () => {
 
     app.post('/', (req, res) => {
         const ip = getVar('pi-ip').value;
-        const cmd = `curl -s -XPOST ${ip}${req.body}`;
+        const cmd = `curl -s -XPOST ${ip}:8080${req.body}`;
         logger.log('[pi]', cmd);
         exec(cmd, {stdio: [0,1,2]}, (error, stdout, stderr) => {
             if (error) return res.sendError(error);
