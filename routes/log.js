5b6bf4786978cb506884730ba8a866c19351753e
Stefan Mirea <steven.mirea@gmail.com>
2016-12-18 12:58:26 -0500

    Added POST to log route

--- routes/log.js
+++ routes/log.js
@@ -2,14 +2,26 @@
 const fs = require('fs');
 const express = require('express');
 const stripAnsi = require('strip-ansi');
-const {LOG_FILE} = require('../utils/logger');
+const logger = require('../utils/logger');
 
 module.exports = () => {
     const app = express.Router();
 
     app.get('/', (req, res) => {
         res.set('Content-Type', 'text/plain');
-        res.send(stripAnsi('' + fs.readFileSync(LOG_FILE)));
+        res.send(
+            stripAnsi(
+                '' + fs.readFileSync(logger.LOG_FILE)
+            )
+            .split('\n')
+            .reverse()
+            .join('\n')
+        );
+    });
+
+    app.post('/', (req, res) => {
+        logger.info(req.body);
+        res.json({success: true});
     });
 
     return app;
